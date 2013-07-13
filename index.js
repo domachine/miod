2749a5f30d19e01c1588758e46a5315935e05e7c
Dominik Burgdörfer <dominik.burgdoerfer@mikanda.de>
2013-07-13 23:54:33 +0200

    implemented drop-list

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 
 var mongoose = require('mongoose')
   , express = require('express');
-mongoose.connect('mongodb://localhost');
+mongoose.connect('mongodb://localhost/test');
 
 // create application
 
@@ -18,6 +18,7 @@ require('./lib/models');
 // setup application
 
 app.use(express.logger('dev'));
+app.use(express.favicon());
 app.use(require('./lib/auth'));
 app.use(require('./lib/miod'));
 app.listen(3000);
